37c4fe5b4c55467cbdac9da85248994ddfdec08e
XxOscar-GarzónxX <garzongtoscar@gmail.com>
2021-07-01 16:48:08 -0500

    Ejercicios Java 1

---
+++ src/clase01.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class clase01 {
+    /*EJERCICIO 1 OPCION 1 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Dame tu nombre");
+        String nombre = scan.nextLine();
+        System.out.println("Hola "+nombre);
+    }*/
+    /*EJERCICIO 1 OPCION 2
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Dame tu nombre: ");
+        String nombre = scan.nextLine();
+        String finale = saludo(nombre);
+        System.out.println(finale);
+    }
+    public static String saludo(String nombre) {
+        return ("Hola "+nombre+"!");
+    }*/
+}
+
