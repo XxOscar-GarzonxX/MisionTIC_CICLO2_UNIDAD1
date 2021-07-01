96c6825999c438328b21fe01464b1757371c78f3
XxOscar-GarzónxX <garzongtoscar@gmail.com>
2021-07-01 17:27:25 -0500

    Borre lineas

--- clase1.java
+++ clase1.java
@@ -12,10 +12,6 @@ public class clase01 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Dame tu nombre: ");
-        String nombre = scan.nextLine();
-        String finale = saludo(nombre);
-        System.out.println(finale);
-        system.out.println("Esto es un cambio");
     }
     public static String saludo(String nombre) {
         return ("Hola "+nombre+"!");
